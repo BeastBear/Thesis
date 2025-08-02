8a0471d24fa7bb6bc25a9772072a21a0d5fca53c
BeastBear <phurich_10@hotmail.com>
2025-08-02 18:17:26 +0700

    Auth0 Access Token, Validate Access Token, Update user profile api

---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import './global.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './AppRoutes'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>,
+)
